b0f9f043d9e405b3d726ab4d0e9bb433a8ce23f3
yeasin Rahaman <ryeashin@gmail.com>
2017-08-06 13:41:31 +0600

    Update fileLists.java

--- Titus/src/fileLists.java
+++ Titus/src/fileLists.java
@@ -1,57 +1,50 @@
 import java.io.*;
 import java.nio.*;
 import java.nio.file.*;
+import java.util.ArrayList;
 
 import javax.crypto.SealedObject;
 import javax.management.ListenerNotFoundException;
+import javax.swing.filechooser.FileSystemView;
 
 import raws.SearchPage;
 
 public class fileLists extends SearchPage {
 
-//	void listFolder(File dir){
-//		File[] subDirs = dir.listFiles(new FileFilter() {
-//			
-//			@Override
-//			public boolean accept(File pathName) {
-//				// TODO Auto-generated method stub
-//				return pathName.isDirectory(); 
-//			}
-//		});
-//		
-//		System.out.println("\nDirectory " + dir.getAbsolutePath());
-//		listFile(dir);
-//		
-//		for(File folder : subDirs){
-//			listFolder(folder);
-//		}
-//	}
-	
-//	private void listFile(File dir) {
-//		// TODO Auto-generated method stub
-//		File[] files = dir.listFiles();
-//		
-//		for(File file: files){
-//			System.out.println(file.getName());
-//		}
-//		
-//	}
+	public  static void listf(String directoryName, ArrayList<File> files) {
+        File directory = new File(directoryName);
+        // files(ArrayList<File>) is ur main list what u will use all the time for accessing files;
+        // get all the files from a directory
+        File[] fList = directory.listFiles();
+        if(fList==null) return;
+        for (File file : fList) {
+            if (file.isFile()) {
 
+                files.add(file);
+//                System.out.println("File --- "+file.getAbsolutePath()); // comment this line out.. its to show files when u test run it
+            } else if (file.isDirectory()) {
+//                System.out.println("Folder --- "+file.getAbsolutePath()); // comment this line out.. its to show files when u test run it
+                listf(file.getAbsolutePath(), files);
+            }
+
+        }
+    }
+	
 	public static void main(String[] args){
-//		new fileLists().listFolder(new File("D:\\"));
-	
-		File files = new File("D:\\");
-		File[] list = files.listFiles();
+		File[] paths;
+        FileSystemView fsv = FileSystemView.getFileSystemView();
 		
-		for(File f : list){
-//			System.out.println(f);
-			if(files.isDirectory()){
-				System.out.println(f);
-			}
+			// returns pathnames for files and directory
+		        paths = File.listRoots();
+		        ArrayList<File> theUltimateFileList = new ArrayList<>();
+		        // for each pathname in pathname array
+		        for(File path:paths){
 		
-		}
-	
-	
+//		            System.out.println(" ------- Drive: "+path);
+		            listf(path.getAbsolutePath(),theUltimateFileList);		
+		
+		        }
+		        
 	}
-	
+
 }
